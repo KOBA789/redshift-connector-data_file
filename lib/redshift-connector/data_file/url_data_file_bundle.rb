35e531b6fc8888c835221319b1354fabdc9f62b7
Hidekazu Kobayashi <hidekazu-kobayashi@cookpad.com>
2017-06-02 17:23:44 +0900

    Disallow empty data_file_urls

--- lib/redshift-connector/data_file/url_data_file_bundle.rb
+++ lib/redshift-connector/data_file/url_data_file_bundle.rb
@@ -6,6 +6,7 @@ require 'redshift-connector/data_file/url_data_file'
 module RedshiftConnector
   class UrlDataFileBundle < AbstractDataFileBundle
     def initialize(data_file_urls, format: :redshift_csv, filter: nil, logger: DataFile.logger)
+      raise ArgumentError, 'data_file_urls is empty' if data_file_urls.empty?
       @data_file_urls = data_file_urls
       @filter = filter || lambda {|*row| row }
       @logger = logger
